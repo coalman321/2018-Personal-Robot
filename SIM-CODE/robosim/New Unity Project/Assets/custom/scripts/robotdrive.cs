09d94762e7d8077f52b68fdfa2565f654ed3f714
coalman321 <coalman321@users.noreply.github.com>
2019-01-20 01:07:31 -0500

    add robot chassis and begin recording / playback

--- SIM-CODE/robosim/New Unity Project/Assets/custom/scripts/robotdrive.cs
+++ SIM-CODE/robosim/New Unity Project/Assets/custom/scripts/robotdrive.cs
@@ -5,45 +5,61 @@ public class robotdrive : MonoBehaviour
 {
     public float conversion;
     public float gravity;
-    public bool isNetworked;
+    public bool isNetworked, isRecorded, isPlayback;
+    public string saveDir, file;
+    
     private NetworkHelper net;
-
     private Rigidbody rb;
     public float speed;
     private float xInitialPosition, yInitialPosition, zInitialPosition, yInitialRotation;
     private float x, z, theta;
+    private int frame, frames;
 
+    public int Frame {
+        get { return frame; }
+        set { frame = value; }
+    }
+    
     // Start is called before the first frame update
     private void Start()
     {
-        net = new NetworkHelper(5800);
+        net = new NetworkHelper(5800, saveDir, 100, NetworkHelper.Mode.Networked);// start in networked mode and adjust as needed
         xInitialPosition = gameObject.transform.position.x;
         yInitialPosition = gameObject.transform.position.y;
         zInitialPosition = gameObject.transform.position.z;
         yInitialRotation = gameObject.transform.rotation.y;
-        Debug.Log(string.Format("X: {0}, Y: {1}, Theta: {2}", xInitialPosition, yInitialPosition, yInitialRotation));
-        if (!isNetworked) rb = GetComponent<Rigidbody>();
+        //Debug.Log(string.Format("X: {0}, Y: {1}, Theta: {2}", xInitialPosition, yInitialPosition, yInitialRotation));
+        
+        if (isNetworked) net.mode = NetworkHelper.Mode.Networked;
+        else if (isRecorded) net.mode = NetworkHelper.Mode.Recording;
+        else if (isPlayback) {
+            net.mode = NetworkHelper.Mode.Playback;
+            frames = net.loadSave(saveDir + "\\" + file);
+            frame = 0;
+        }
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (isNetworked)
+        if (net.mode == NetworkHelper.Mode.Playback) {
+            net.update(frame);
+            x = xInitialPosition - net.getX() / conversion;
+            z = zInitialPosition - net.getY() / conversion;
+            theta = yInitialRotation - net.getTheta();
+            gameObject.transform.position = new Vector3(x, yInitialPosition, z); // y is vertical in unity but not in pose
+            gameObject.transform.rotation = Quaternion.Euler(0.0f, theta, 0.0f); //rotation around vertical axis
+            frame ++;
+        }
+        else if (net.mode == NetworkHelper.Mode.Recording || net.mode == NetworkHelper.Mode.Networked)
         {
-            net.update();
+            net.update(0); // frame index is unused therefore zero
             x = xInitialPosition - net.getX() / conversion;
             z = zInitialPosition - net.getY() / conversion;
             theta = yInitialRotation - net.getTheta();
             gameObject.transform.position = new Vector3(x, yInitialPosition, z); // y is vertical in unity but not in pose
             gameObject.transform.rotation = Quaternion.Euler(0.0f, theta, 0.0f); //rotation around vertical axis
             //Debug.Log(string.Format("X: {0}, Y: {1}, Theta: {2}", x, y, theta));
-        }
-        else
-        {
-            var horiz = -Input.GetAxis("Vertical");
-            var vert = Input.GetAxis("Horizontal");
-            var move = new Vector3(horiz, gravity / speed, vert);
-            rb.AddForce(move * Time.deltaTime * speed);
         }
     }
 

f5f49e3f8cc9c18fa4198919fac1ae88f25dde6f
coalman321 <coalman321@users.noreply.github.com>
2019-04-05 22:09:03 -0400

    working on socket tables client
    ther eis an issue with the server closing its connections after finishing

--- RIO-CODE/official/src/main/java/frc/lib/util/SocketTables.java
+++ RIO-CODE/official/src/main/java/frc/lib/util/SocketTables.java
@@ -1,20 +1,82 @@
 package frc.lib.util;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 public class SocketTables {
 
     private static final SocketTables m_Instance = new SocketTables();
-    private static final String SOCKET_TABLES_IP = "";
+    private static final String SOCKET_TABLES_IP = "127.0.0.1";
+    private Socket client;
+    private SocketAddress serverAddress;
+    private DataOutputStream outToServer;
+    private BufferedReader inFromServer;
+    private boolean initSuccess = false;
+    private SocketTables() {
+        try {
+            client = new Socket();
+            serverAddress = new InetSocketAddress(SOCKET_TABLES_IP, 7777);
+            client.connect(serverAddress);
+            outToServer = new DataOutputStream(client.getOutputStream());
+            inFromServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            initSuccess = true;
+        } catch (IOException except) {
+            System.out.println("SocketTables initialization failure");
+            except.printStackTrace();
+        }
 
-    public static SocketTables getInstance(){
+    }
+
+    public static SocketTables getInstance() {
         return m_Instance;
     }
 
-    private Socket client;
+    public String queryValue(String request) {
+        System.out.println(client.isConnected());
+        if (!initSuccess) return null;
+        try {
+            if (!client.isConnected()) client.connect(serverAddress);
+            writeSocket(request);
+            final String query = readSocket();
+            System.out.println("query succeeded");
+            return query;
+        } catch (IOException e) {
+            System.out.println("Query failed");
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-    private SocketTables(){
-        client = new Socket(SOCKET_TABLES_IP, 7777);
+    private String readSocket() throws IOException {
+        return inFromServer.readLine();
+    }
+
+    private void writeSocket(String toWrite) throws IOException {
+
+        outToServer.write(toWrite.getBytes());
+        outToServer.flush();
+
+    }
+
+    private void close() {
+
+    }
+
+    private enum DataOperation {
+
+        GET("GET"),
+        GETALL("GETALL"),
+        UPDATE("UPDATE"),
+        DELETE("DELETE");
+
+        private DataOperation(String opcode) {
+
+        }
 
     }
 
--- RIO-CODE/official/src/main/java/frc/robot/Robot.java
+++ RIO-CODE/official/src/main/java/frc/robot/Robot.java
@@ -1,57 +1,68 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.lib.util.SocketTables;
 
 public class Robot extends TimedRobot {
 
-  @Override
-  public void robotInit() {
+    @Override
+    public void robotInit() {
+        System.out.println("Q1:" + SocketTables.getInstance().queryValue("{\"request\": \"UPDATE\", \"key\": \"test1\", \"value\": 4}"));
+        System.out.println("Q2:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+        System.out.println("Q3:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+        System.out.println("Q4:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+        System.out.println("Q5:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+        System.out.println("Q6:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+        System.out.println("Q7:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+        System.out.println("Q8:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+        System.out.println("Q9:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+        System.out.println("Q10:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+        System.out.println("Q11:" + SocketTables.getInstance().queryValue("{\"request\": \"GET\", \"key\": \"test1\"}"));
+    }
 
-  }
+    @Override
+    public void robotPeriodic() {
 
-  @Override
-  public void robotPeriodic() {     
+    }
 
-  }
+    @Override
+    public void disabledInit() {
 
-  @Override
-  public void disabledInit() {
+    }
 
-  }
+    @Override
+    public void disabledPeriodic() {
 
-  @Override
-  public void disabledPeriodic() {
+    }
 
-  }
+    @Override
+    public void autonomousInit() {
 
-  @Override
-  public void autonomousInit() {
+    }
 
-  }
+    @Override
+    public void autonomousPeriodic() {
 
-  @Override
-  public void autonomousPeriodic() {
+    }
 
-  }
+    @Override
+    public void teleopInit() {
 
-  @Override
-  public void teleopInit() {
+    }
 
-  }
+    @Override
+    public void teleopPeriodic() {
 
-  @Override
-  public void teleopPeriodic() {
-      
-  }
+    }
 
-  @Override
-  public void testInit() {
+    @Override
+    public void testInit() {
 
-  }
+    }
 
-  @Override
-  public void testPeriodic() {
+    @Override
+    public void testPeriodic() {
 
-  }
+    }
 
 }
